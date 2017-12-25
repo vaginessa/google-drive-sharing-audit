bb09588edfbb031abd614d63b5428a2ed2557d12
Andrew Gu <1711729+a-gu@users.noreply.github.com>
2017-12-24 18:31:16 -0600

    Separate client JavaScript

--- web.gs.js
+++ web.gs.js
@@ -1,8 +1,14 @@
 // Webserver
 // Functions to handle serving resources to client
 
-// doGet  NONE -> NONE
+// doGet  NONE -> Google@HTML
 // Returns "index.html" page for all requests
 function doGet () {
-  return HtmlService.createHtmlOutputFromFile('index.html')
+  return HtmlService.createTemplateFromFile('index.html').evaluate()
+}
+
+// include  String -> Google@HTML
+// Function used for including other HTML files in templates
+function include (filename) {
+  return HtmlService.createHtmlOutputFromFile(filename).getContent()
 }
